663b473da7d6e63319ec34826aa28f337007201e
bogsdev <bogs.for.dev@gmail.com>
2020-01-03 11:12:54 +0800

    First Commit

---
+++ android/app/src/main/kotlin/balangat/robert/com/git_commit_history/MainActivity.kt
@@ -0,0 +1,12 @@
+package balangat.robert.com.git_commit_history
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
